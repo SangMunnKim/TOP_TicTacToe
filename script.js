81391903e4c4e595e4f0767cb97f34e7f66a2c3a
Sang <108661792+SangMunnKim@users.noreply.github.com>
2024-07-11 12:52:08 +0200

    add gameboard, displaycontroller and checkwin functions

--- script.js
+++ script.js
@@ -1,17 +1,78 @@
 const gameBoard = (function(){
+    // Create an array to represent the game board
+    const board = Array(9).fill("");
 
-    const board = Array(9).fill(null);
+    const getBoard = () => board;
+
+    const setMarker = (index, marker) => {
+        if (board[index] === "") {
+          board[index] = marker;
+          return true;
+        }
+        return false;
+      };
+
+      const resetBoard = () => {
+        board = ["", "", "", "", "", "", "", "", ""];
+      };
 
 
-    return {
-        GetBoard: () => board,
-        UpdateBoard: (index, value) => board[index] = value,
-    }
+    return { getBoard, setMarker, resetBoard }
 })();
 
-function checkWin() {
+// DisplayController module
+const displayController = (function() {
+    const printBoard = () => {
+      const board = gameBoard.getBoard();
+      console.log(`${board[0]} | ${board[1]} | ${board[2]}`);
+      console.log("---------");
+      console.log(`${board[3]} | ${board[4]} | ${board[5]}`);
+      console.log("---------");
+      console.log(`${board[6]} | ${board[7]} | ${board[8]}`);
+    };
+  
+    return { printBoard };
+})();
 
+const Player = (name, marker) => {
+    return { name, marker };
 }
+
+const checkwin = (function checkWin() {
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    function check() {
+        let roundWon = false;
+        for (let i = 0; i <= 7; i++) {
+            const winCondition = winningConditions[i];
+            const gameState = gameBoard.getBoard();
+            let a = gameState[winCondition[0]];
+            let b = gameState[winCondition[1]];
+            let c = gameState[winCondition[2]];
+    
+            if (a === '' || b === '' || c === '') {
+                continue;
+            }
+
+            if (a === b && b === c) {
+                roundWon = true;
+                break
+            }
+        }
+        return roundWon;
+    }
+
+    return { check }
+})();
 
 function handleClick(event) {
 
